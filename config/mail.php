7c978b3310cf83375c1273dcc3126d7625957ba5
KoplarKoprol <putu.moreno298@gmail.com>
2026-07-09 17:33:23 +0800

    add: email config template file

---
+++ config/mail.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'host' => 'smtp.gmail.com',
+    'port' => 587,
+    'username' => 'helpdesk.stikom@gmail.com', //isi dengan email anda!
+    'password' => 'ganti_dengan_app_password',
+    'from_email' => 'helpdesk.stikom@gmail.com', //isi dengan email anda!
+    'from_name' => 'Helpdesk IT STIKOM Bali',
+];
